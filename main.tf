674695d3c56f5802a6eca4470ca84feb97bf6507
Niels Bischof <nbischof@gmail.com>
2023-09-20 15:00:02 -0400

    uncommented acl as it seems not to be supported.

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ EOF
 }
 
 resource "aws_s3_object" "webapp" {
-  acl          = "public-read"
+  # acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
   content      = file("${path.module}/assets/index.html")
